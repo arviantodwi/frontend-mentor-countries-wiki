9669bae3a62164d23101f78f8c1296fa783a72b4
Arvianto D. Wicaksono <dev@arvian.to>
2025-09-26 11:08:44 +0700

    test: add vitest and testing library configuration

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,5 +1,7 @@
 import { cleanup } from '@testing-library/react';
-import { afterEach } from 'vitest';
+import { afterEach, vi } from 'vitest';
+
+vi.mock('zustand');
 
 afterEach(() => {
   cleanup();
