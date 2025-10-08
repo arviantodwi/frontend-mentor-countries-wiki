de48521e068e5297100f3d2606ae0972b08a0ff7
Arvianto D. Wicaksono <dev@arvian.to>
2025-10-08 14:46:33 +0700

    chore: add path alias configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import path from 'node:path';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@/features': path.resolve(__dirname, './src/features'),
+      '@/shared': path.resolve(__dirname, './src/shared'),
+      '@/types': path.resolve(__dirname, './src/types'),
+    },
+  },
 });
